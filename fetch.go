5770712a7eb3149175ba4cdbfac86a4900e0349e
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-07-28 17:37:08 +0200

    feat: fetch RSS feed

---
+++ fetch.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Language    string    `xml:"language"`
+		Items       []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchRSS(url string) (*RSSFeed, error) {
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+	res, err := client.Get(url)
+    if err != nil {
+        return nil, err
+    }
+    defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Read error: %v", err)
+	}
+
+	var rssFeed RSSFeed
+	err = xml.Unmarshal(data, &rssFeed)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rssFeed, nil
+}
